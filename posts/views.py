4c14931e0bbfff38ae73de945ec286caa24b1944
I.MaN <jahediman@gmail.com>
2023-07-10 09:07:52 +0330

    added custom permission

--- posts/views.py
+++ posts/views.py
@@ -2,14 +2,16 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 from .models import Posts
 from .serializers import PostsSerializer
-
+from .permissions import IsAuthorOrReadOnly
 
 # Create your views here.
 class PostListApiView(ListCreateAPIView):
     queryset = Posts.objects.all()
     serializer_class = PostsSerializer
+    permission_classes = IsAuthorOrReadOnly,
 
 
 class PostDetailApiView(RetrieveUpdateDestroyAPIView):
     queryset = Posts.objects.all()
+    permission_classes = IsAuthorOrReadOnly,
     serializer_class = PostsSerializer
